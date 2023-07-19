afa60e908c566711b7990dad4b0270d49e045e5b
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-07-19 18:19:31 -0300

    feat: creating product page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,14 @@ import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from '@/lib/stripe'
 import Stripe from 'stripe'
+import Link from 'next/link'
 
 interface HomeProps {
   products: {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 
@@ -24,17 +25,25 @@ export default function Home({ products }: HomeProps) {
       spacing: 48,
     },
   })
+
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link
+            href={`/product/${product.id}`}
+            key={product.id}
+            prefetch={false}
+          >
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
@@ -53,11 +62,9 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-
       price: new Intl.NumberFormat('pt-BR', {
         style: 'currency',
         currency: 'BRL',
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       }).format(price.unit_amount! / 100),
     }
   })
@@ -66,6 +73,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: {
       products,
     },
-    revalidate: (60 * 60) / 2,
+    revalidate: 60 * 60 * 2, // 2 hours,
   }
 }
---
+++ src/styles/pages/product.ts
@@ -0,0 +1,66 @@
+import { styled } from '..'
+
+export const ProductContainer = styled('main', {
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  alignItems: 'stretch',
+  gap: '4rem',
+  margin: '0 auto',
+  maxWidth: 1180,
+})
+
+export const ImageContainer = styled('div', {
+  width: '100%',
+  maxWidth: 576,
+  height: 656,
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  padding: '0.25rem',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  img: {
+    objectFit: 'cover',
+  },
+})
+
+export const ProductDetails = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray300',
+  },
+
+  span: {
+    display: 'block',
+    marginTop: '1rem',
+    fontSize: '$2xl',
+    color: '$green300',
+  },
+
+  p: {
+    marginTop: '2.5rem',
+    color: '$gray300',
+    lineHeight: 1.6,
+    fontSize: '$md',
+  },
+
+  button: {
+    marginTop: 'auto',
+    background: '$green500',
+    border: 0,
+    color: '$white',
+    borderRadius: 8,
+    padding: '1.25rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontSize: '$md',
+
+    '&:hover': {
+      background: '$green300',
+    },
+  },
+})
