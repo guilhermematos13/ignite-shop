0f6426a8ed2249f8b8c60735f00053ee93017a9f
Guilherme Matos <guilhermemoliveira1998@gmail.com>
2023-07-19 20:11:07 -0300

    feat: creating success layout

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,17 @@
+import { ImageContainer, SuccessContainer } from '@/styles/pages/success'
+import Link from 'next/link'
+
 export default function Success() {
   return (
-    <div>
-      <h1>Success</h1>
-    </div>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+      <ImageContainer></ImageContainer>
+      <p>
+        Uhuul <strong>Diego Fernandes</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits </strong>
+        já está a caminho da sua casa.
+      </p>
+      <Link href="/">Voltar ao catalogo</Link>
+    </SuccessContainer>
   )
 }
